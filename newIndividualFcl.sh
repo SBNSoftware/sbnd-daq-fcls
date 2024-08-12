6ab70bf8e90da67bdc2c7dca0fa472ca63535c6e
sbnd <sbnd@fnal.gov>
2024-08-12 11:04:22 -0500

    add a script in case you need to add an entirely new fcl to all of the existing configurations (like if theres a new feb fcl for the CRTs cause they swapped a board)

---
+++ newIndividualFcl.sh
@@ -0,0 +1,37 @@
+if [ $# -eq 0 ] #check if there were no arguments
+then                                                                                            
+    echo "Please give the new fcl name (without .fcl at the end) as an argument, and make sure it exists in standard already. eg. feb123"
+    return 1
+fi                                                                                              
+
+
+
+
+ALLDIRS=$(ls -d */)
+FNAME=$1
+echo "Add fcl to list_default_fcls.txt"
+$FNAME >> list_default_fcls.txt
+PWD1=$PWD #remember where we started so we can return there when done
+
+
+
+for DIR in $ALLDIRS; do 
+    echo $DIR
+    if [ $DIR == "standard/" ]
+    then
+	continue
+    fi
+    cd $DIR
+    ln -s ../../standard/${FNAME}.fcl defaults/${FNAME}_default.fcl #create softlinks to all the fcls in standard 
+    #(two up directories since its relative to the "<configname>/defaults directory not the current one)
+
+    INCLUDE='#include "'${FNAME}'_default.fcl"'
+    echo $INCLUDE > ${FNAME}.fcl  #create a skeleton file that just includes the _default.fcl soft link
+    echo "#======Place override parameters below=========" >>${FNAME}.fcl
+    mv feb*.fcl febs #clean up the list of fcls a bit
+    cd $PWD1
+done
+
+
+
+echo "Symbolic links to configs/standard and editable skeleton fcl added to each directory"
