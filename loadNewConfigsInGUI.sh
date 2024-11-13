e47a2cb34a4b5d15b30541debf3d9b6db6060fbd
Sbnd <sbnddaq@sbnd-evb01.fnal.gov>
2024-11-13 14:53:56 -0600

    turn PMT metric persistance on Associated database configurations: fullCommTriggerMenuEW-NS2-00008 fullCommTriggerMenuNS2-00008 nsCrossingMuonFlash2-00008 ewCrossingMuonFlash2-00008 bnbZeroBias2-00008 ewCrossingMuon2-00008 nsCrossingMuon2-00008 offbeamZeroBias2-00008 offBeamZeroBiasFlash2-00008 caenPUSHbnbZeroBias2-00008

--- loadNewConfigsInGUI.sh
+++ loadNewConfigsInGUI.sh
@@ -2,7 +2,8 @@
 
 
 HOMEDIR=/tmp/artdaq-runcontrol-gui/db/
-ALLCONFS=$1 #("fullCommTriggerMenuEW-NS8Hz2-00002" "fullCommTriggerMenuNS2-00006" "nsCrossingMuonFlash2-00006" "ewCrossingMuonFlash2-00006" "bnbZeroBias2-00006" "ewCrossingMuon2-00006" "nsCrossingMuon2-00006" "offbeamZeroBias2-00006" "offBeamZeroBiasFlash2-00006" "caenPUSHbnbZeroBias2-00006") ##$1 #
+ALLCONFS=$1 
+#("fullCommTriggerMenuEW-NS8Hz2-00002" "fullCommTriggerMenuNS2-00006" "nsCrossingMuonFlash2-00006" "ewCrossingMuonFlash2-00006" "bnbZeroBias2-00006" "ewCrossingMuon2-00006" "nsCrossingMuon2-00006" "offbeamZeroBias2-00006" "offBeamZeroBiasFlash2-00006" "caenPUSHbnbZeroBias2-00006") ##$1 #
 
 #$(cat list_database_configs.txt)
 echo "ALLCONFS list:" ${ALLCONFS[@]}
